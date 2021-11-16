1a1e553991a26aca7e406e37ada7ce6f5e5a0e97
NinjaStep <r.paragas@outlook.com>
2021-11-16 01:41:07 -0700

    finished models/Category

--- models/Category.js
+++ models/Category.js
@@ -2,11 +2,26 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Category extends Model {}
+class Category extends Model { }
 
 Category.init(
   {
     // define columns
+
+    //define id
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    //define category_name
+    category_name: {
+
+      type: DataTypes.STRING,
+      allowNull: false,
+
+    }
   },
   {
     sequelize,
